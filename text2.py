08d8818d16db3c5e1348462b80bc344add74d82f
Pritam Patro <98096616+pritam-patro@users.noreply.github.com>
2023-07-30 18:26:58 +0530

    Update

--- text2.py
+++ text2.py
@@ -8,7 +8,6 @@ seaborn.set()
 latitude, longitude = cities["latd"], cities["longd"]
 population, area = cities["population_total"], cities["area_total_km2"]
 
-# now we will craete a legend, we will plot empty lists with the desired size and label
 for area in [100, 300, 500]:
     plt.scatter([latitude], [longitude], c='k', alpha=0.3, s=area, label=str(area) + 'km$^2$')
 plt.legend(scatterpoints=1, frameon=False, labelspacing=1, title='City Areas')
